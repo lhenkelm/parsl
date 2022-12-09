a6ffef2c9266a2f696f70fd6931451753979bb7d
Lars Henkelmann <lars.henkelmann@cern.ch>
2022-12-09 18:00:24 +0000

    since we want to use it postionally ~all the time, allow that

--- parsl/ipv6.py
+++ parsl/ipv6.py
@@ -78,7 +78,7 @@ def ip_version_from_optional(addresses : Sequence[Union[str, None]]) -> str:
     return DEFAULT_IP_VERSION
 
 
-def context(*args, ip_version : str = 'IPv6', **kwargs) -> zmq.Context:
+def context(ip_version : str = 'IPv6', *args, **kwargs) -> zmq.Context:
   """
   0MQ Context factory that enables IPv6 by default.
   """
